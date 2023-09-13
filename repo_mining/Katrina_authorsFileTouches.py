73a33b0825badddd04aa4b6ad0e95d59b441e640
Benek6h <143664609+Benek6h@users.noreply.github.com>
2023-09-13 13:17:34 -0700

    Removed token from Katrina_authorsFileTouches.py

--- repo_mining/Katrina_authorsFileTouches.py
+++ repo_mining/Katrina_authorsFileTouches.py
@@ -99,7 +99,7 @@ repo = 'scottyab/rootbeer'
 # Remember to empty the list when going to commit to GitHub.
 # Otherwise they will all be reverted and you will have to re-create them
 # I would advise to create more than one token for repos with heavy commits
-lstTokens = ["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"]
+lstTokens = [""]
 
 # open the file before calling the function
 file = repo.split('/')[1]
